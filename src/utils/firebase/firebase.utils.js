20028a5467972e23796d9a3e7a195dfa0ad28b82
George-Valentin Cujbă <george.cujba15@gmail.com>
2024-08-15 07:22:56 +0300

    Fix .env firebase configurations to use each property separately instead of a JSON object

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -22,7 +22,14 @@ import {
   getDocs,
 } from "firebase/firestore";
 
-const firebaseConfig = JSON.parse(process.env.FIRESBASE_FIRESTORE_CONFIG_INFO);
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
